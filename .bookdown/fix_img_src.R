45927375ddfc44b4b80a016c2180c346f68de71c
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2018-08-22 15:47:00 +0800

    config bookdown

--- .bookdown/fix_img_src.R
+++ .bookdown/fix_img_src.R
@@ -3,5 +3,11 @@ for ( md_file in yaml::read_yaml('_bookdown.yml')$rmd_files) {
     content = readr::read_file(md_file)
     content = stringr::str_remove_all(content, '(?<=\\]\\()(\\.\\./)+')
     content = stringr::str_replace_all(content, '/?\\.gitbook/assets', 'images')
+    if (basename(md_file) != 'index.Rmd')
+        content = stringr::str_replace_all(content, '^---$', '------')
     readr::write_file(content, md_file)
 }
+
+readr::write_lines('# Linux', 'part-i.-basic-tools/1.linux.md')
+
+
