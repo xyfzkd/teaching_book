67cde13de22856d5eeabd49e8d1cd1ea0d2080ee
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2018-08-22 16:11:04 +0800

    fix as_bookdown.R

--- .bookdown/as_bookdown.R
+++ .bookdown/as_bookdown.R
@@ -1,17 +1,19 @@
 library(magrittr)
 for ( md_file in yaml::read_yaml('_bookdown.yml')$rmd_files) {
-    content = readr::read_file(md_file)
+    content = readr::read_lines(md_file)
     # fix image src
     content = stringr::str_remove_all(content, '(?<=\\]\\()(\\.\\./)+')
     content = stringr::str_replace_all(content, '/?\\.gitbook/assets', 'images')
     # use ------ for horizontal line
-    if (basename(md_file) != 'index.Rmd')
+    if (basename(md_file) != 'index.Rmd') {
         content = stringr::str_replace_all(content, '^---$', '------')
+    }
     # 
-    if (basename(md_file) == 'README.md')
-    content[1] = paste0(content[1], ' {#index}')
+    if (basename(md_file) == 'README.md') {
+        content[1] = paste0(content[1], ' {#index}')
+    }
 
-    readr::write_file(content, md_file)
+    readr::write_lines(content, md_file)
 }
 
 readr::write_lines('# Linux', 'part-i.-basic-tools/1.linux.md')
